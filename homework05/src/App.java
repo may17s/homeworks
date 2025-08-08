b053a3359c9eb148e828dee0315038c44dcb34e9
may <170480@bk.ru>
2025-08-08 17:24:28 +0300

    pull request homework5Addition 02

--- homework05/src/App.java
+++ homework05/src/App.java
@@ -1,23 +1,39 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        TelevisionSet tvSet1 = new TelevisionSet();
-        System.out.println(tvSet1);
+        try (Scanner scanner = new Scanner(System.in)) {
+            TelevisionSet[] tvsetArray = new TelevisionSet[10];
+            Random random = new Random();
+            int[] screenSizes = {24, 25, 27, 32};
+            String[] models = {"X1000", "UltraHD", "OLED55", "QLED4K", "SmartView"};
 
-        TelevisionSet tvSet2 = new TelevisionSet(27, TelevisionSet.DisplayTechnology.LED, "Philips", "273s", false);
-        System.out.println(tvSet2);
+            for (int i = 0; i < tvsetArray.length; i++) {
+                System.out.println("\n");
+                System.out.println("Введите данные для телевизора " + (i + 1) + ":");
+                System.out.print("ТВ№" + (i + 1) + ", Марка: ");
+                String brandName = scanner.nextLine();
+                System.out.print("ТВ№" + (i + 1) + ", Поддерживает Smart TV? (true/false): ");
+                boolean hasSmartTV = scanner.nextBoolean();
+                System.out.print("ТВ№" + (i + 1) + ", Номер текущего канала: ");
+                int currentChannel = scanner.nextInt();
+                System.out.print("ТВ№" + (i + 1) + ", Громкость звука (от 0 до 100): ");
+                int volume = scanner.nextInt();
+                System.out.print("ТВ№" + (i + 1) + ", Телевизор включен? (true/false): ");
+                boolean tvSwitch = scanner.nextBoolean();
 
-        Random random = new Random();
-        int[] screenSizes = {24, 25, 27, 32};
-        String[] brands = {"Samsung", "LG", "Sony", "Philips", "Panasonic"};
-        String[] models = {"X1000", "UltraHD", "OLED55", "QLED4K", "SmartView"};
-
-        TelevisionSet tvSet3 = new TelevisionSet(
-                screenSizes[random.nextInt(screenSizes.length)],
-                TelevisionSet.DisplayTechnology.values()[random.nextInt(TelevisionSet.DisplayTechnology.values().length)],
-                brands[random.nextInt(brands.length)], models[random.nextInt(models.length)], random.nextBoolean());
-        System.out.println(tvSet3);
+                TelevisionSet tvSet = new TelevisionSet(
+                        screenSizes[random.nextInt(screenSizes.length)],
+                        TelevisionSet.DisplayTechnology.values()[random.nextInt(TelevisionSet.DisplayTechnology.values().length)],
+                        brandName, models[random.nextInt(models.length)], random.nextBoolean());
+                tvSet.setCurrentChannel(currentChannel);
+                tvSet.setVolume(volume);
+                if (tvSwitch) tvSet.setTvSwitch();
+                tvsetArray[i] = tvSet;
+                System.out.println(tvSet);
+            }
+        }
     }
 }
