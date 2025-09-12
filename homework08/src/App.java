f01e4af0dee1f8a68c296c5bbe3b4bbf5b240fa5
may <170480@bk.ru>
2025-09-12 15:55:20 +0300

    Реализуем чтение файла input.txt

--- homework08/src/App.java
+++ homework08/src/App.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        try {
+            Path filePath = Paths.get("src/input.txt");
+            List<String> lines = Files.readAllLines(filePath);
+
+            for (String line : lines) {
+                line = line.trim();
+
+                if (line.isEmpty()) continue;
+
+                if (line.equals("END")) {
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при работе с файлом: " + e.getMessage());
+        }
     }
 }
