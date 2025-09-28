ed5d209520be3ffef059fe66c7f02c245a160292
may <170480@bk.ru>
2025-09-28 22:32:21 +0300

    Создаём DragRace.java

---
+++ homework09/src/model/DragRace.java
@@ -0,0 +1,9 @@
+package model;
+
+public class DragRace extends Race {
+    public DragRace() { super(); }
+
+    public DragRace(int length, String route, int prize) {
+        super(length, route, prize);
+    }
+}
