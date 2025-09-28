9556cde72af408c35e6e1893530d0abe54d8da95
may <170480@bk.ru>
2025-09-28 22:30:50 +0300

    Создаём CasualRace.java

---
+++ homework09/src/model/CasualRace.java
@@ -0,0 +1,9 @@
+package model;
+
+public class CasualRace extends Race {
+    public CasualRace() { super(); }
+
+    public CasualRace(int length, String route, int prize) {
+        super(length, route, prize);
+    }
+}
