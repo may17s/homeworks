6b3dc8538ac5fbaf4bdb112ebf0b8cb92c1d0a19
may <170480@bk.ru>
2025-11-12 15:27:24 +0300

    Создаём spring maven проект com.may.a03

---
+++ attestation03/src/main/java/com/may/a03/A03Application.java
@@ -0,0 +1,11 @@
+package com.may.a03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A03Application {
+	public static void main(String[] args) {
+		SpringApplication.run(A03Application.class, args);
+	}
+}
