5650fceedc409a745721bced79a3f7fb9a73b4e2
may <170480@bk.ru>
2025-11-28 11:15:17 +0300

    Добавляем описание аннотации swagger для контроллеров

--- attestation03/src/main/java/com/may/a03/controller/MedicalNoteController.java
+++ attestation03/src/main/java/com/may/a03/controller/MedicalNoteController.java
@@ -2,6 +2,10 @@ package com.may.a03.controller;
 
 import com.may.a03.dto.MedicalNoteDto;
 import com.may.a03.service.MedicalNoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Медицинские записи", description = "Операции с медицинскими записями")
 @RestController
 @RequestMapping("/api/notes")
 public class MedicalNoteController {
@@ -18,36 +23,50 @@ public class MedicalNoteController {
         this.service = service;
     }
 
+    @Operation(summary = "Получение списка всех активных медицинских записей")
     @GetMapping
     @ResponseBody
     public List<MedicalNoteDto> getAll() {
         return service.getAllActive();
     }
 
+    @Operation(summary = "Получение конкретной медицинской записи")
     @GetMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ResponseEntity<MedicalNoteDto> getById(@PathVariable Long id) {
+    public ResponseEntity<MedicalNoteDto> getById(@Parameter(
+            description = "ID медицинской записи",
+            required = true) @PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+    @Operation(description = "Объект медицинская запись", summary = "Создание новой медицинской записи")
     @PostMapping
+    @ApiResponse(responseCode = "201", description = "Медицинская запись успешно создана")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public MedicalNoteDto create(@Valid @RequestBody MedicalNoteDto dto) {
+    public MedicalNoteDto create(@RequestBody @Valid MedicalNoteDto dto) {
         return service.create(dto);
     }
 
+    @Operation(summary = "Удаление конкретной медицинской записи")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    @ApiResponse(responseCode = "204", description = "Медицинская запись успешно удалена")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> delete(@Parameter(
+            description = "ID удаляемой медицинской записи",
+            required = true) @PathVariable Long id) {
         service.softDelete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Объект медицинская запись, которая должен быть обновлёна",
+            summary = "Изменение конкретной медицинской записи")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<MedicalNoteDto> update(
-            @PathVariable Long id,
+    public ResponseEntity<MedicalNoteDto> update(@Parameter(
+            description = "ID медицинской записи",
+            required = true) @PathVariable Long id,
             @RequestBody @Valid MedicalNoteDto dto) {
         MedicalNoteDto updated = service.update(id, dto);
         return ResponseEntity.ok(updated);
--- attestation03/src/main/java/com/may/a03/controller/PatientController.java
+++ attestation03/src/main/java/com/may/a03/controller/PatientController.java
@@ -1,16 +1,17 @@
 package com.may.a03.controller;
 
-import com.may.a03.dto.MedicalNoteDto;
 import com.may.a03.dto.PatientDto;
-import com.may.a03.service.MedicalNoteService;
 import com.may.a03.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Пациенты", description = "Операции с пациентами")
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
@@ -20,16 +21,20 @@ public class PatientController {
         this.service = service;
     }
 
+    @Operation(summary = "Получение списка всех пациентов", tags = "Пациенты")
     @GetMapping
     @ResponseBody
     public List<PatientDto> getAll() {
         return service.getAllPatients();
     }
 
+    @Operation(description="Объект пациент", summary = "Создание новой записи о пациенте", tags = "Пациенты")
     @PostMapping
+    @ApiResponse(responseCode = "201", description = "Новая запись о пациенте успешно создана")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public PatientDto create(@RequestBody PatientDto dto) {
+    public PatientDto create(@RequestBody
+                @Valid PatientDto dto) {
         return service.create(dto);
     }
 }
