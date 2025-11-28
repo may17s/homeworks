5448747012feb064f66c7b901c9cdbb3d1a5bef8
may <170480@bk.ru>
2025-11-28 11:06:48 +0300

    Описываем классы dto в dto/*

--- attestation03/src/main/java/com/may/a03/dto/MedicalNoteDto.java
+++ attestation03/src/main/java/com/may/a03/dto/MedicalNoteDto.java
@@ -1,13 +1,20 @@
 package com.may.a03.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
+@Schema(description = "Сущность медицинская запись")
 public class MedicalNoteDto {
+    @Schema(description = "Уникальный идентификатор медицинской записи", example = "3", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+    @Schema(description = "Ссылка на пациента", example = "6")
     private Long patientId;
+    @Schema(description = "Ссылка на заболевания", example = "5")
     private Long diseaseId;
+    @Schema(description = "Дата медицинской записи", example = "2025-10-10")
     private LocalDate noteDate;
+    @Schema(description = "Содержимое медицинской записи", example = "Прохождение диспансеризации. Все показатели в пределах нормы.")
     private String note;
 }
--- attestation03/src/main/java/com/may/a03/dto/PatientDto.java
+++ attestation03/src/main/java/com/may/a03/dto/PatientDto.java
@@ -1,17 +1,26 @@
 package com.may.a03.dto;
 
-import jakarta.persistence.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.annotations.Comment;
-
-import java.time.LocalDate;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Сущность пациент")
 public class PatientDto {
+    @Schema(description = "Уникальный идентификатор пациента", example = "2", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+    @Schema(description = "ФИО", example = "Лебедев Алексей Юрьевич")
     private String fullName;
+    @Schema(description = "Дата рождения", example = "1995-12-03")
     private java.time.LocalDate birthDate;
+    @Schema(description = "Пол", allowableValues = {"М", "Ж"})
     private String gender;
+    @Schema(description = "Номер телефона", example = "+79344445566")
     private String phone;
+    @Schema(description = "Адрес", example = "г. Самара, ул. Гагарина, д.22")
     private String address;
 }
